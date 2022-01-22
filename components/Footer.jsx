b184b599976f1c394fdcc008ff8a491be08c3c83
sneha-chavan <snehachavan115@gmail.com>
2022-01-22 17:06:47 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div>
+            Footbar
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+
+function Layout() {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
